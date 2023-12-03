88897947e0415ea1cd59c622c434bf4c09241edb
Ekam <ekamjot.nmed@gmail.com>
2023-12-03 19:54:43 -0400

    removed env var

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: "jattinmood4321@gmail.com",
-    pass: process.env.MAIL_APP_PASSWORD
+    pass: "ajae piox gswz gurp"
   },
 });
 
